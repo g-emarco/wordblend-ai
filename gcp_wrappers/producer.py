fbf18b4a4e6161cb396a51a8c5a65e990508d211
emarco <emarco@google.com>
2023-02-07 17:23:18 +0200

    working cloud run with pubsub and firestore

--- gcp_wrappers/producer.py
+++ gcp_wrappers/producer.py
@@ -7,17 +7,17 @@ from google.cloud import pubsub_v1
 
 TOPIC_NAME = "words"
 
-# logger = logging.getLogger()
+logger = logging.getLogger()
 
 def publish_word(
     email: str, word: str, word_document_id: str, credentials: Credentials
 ) -> None:
-    print(f"publish_word enter")
+    logger.info(f"publish_word enter")
     if not os.environ.get("PRODUCTION"):
         publisher = pubsub_v1.PublisherClient(credentials=credentials)
     else:
         publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(os.environ.get("GCP_PROJECT"), TOPIC_NAME)
     message = {"email": email, "word": word, "word_document_id": word_document_id}
-    print(f"publishing {message=} to topic {topic_path}")
+    logger.info(f"publishing {message=} to topic {topic_path}")
     publisher.publish(topic_path, data=str.encode(str(message)))
